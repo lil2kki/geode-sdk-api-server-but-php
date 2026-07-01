267dc48f5a787f49022019c8d60fe67a64749e05
lil2kki <latterrarity70@gmail.com>
2026-07-01 15:41:29 +0400

    Add configuration file with constants and settings

---
+++ _config.php
@@ -0,0 +1,10 @@
+<?php
+const CLIENT_ID = 'meows';
+const CLIENT_SECRET = 'meows';
+const CALLBACK_URL = 'https://open-geode-index.bccst.ru/callback';
+$ADMIN_USERS = ['lil2kki'];
+
+const UPSTREAM_API = 'https://api.geode-sdk.org';
+
+const ICON_URL = 'https://raw.githubusercontent.com/lil2kki/Open-Geode-Index/HEAD/logo.png';
+const SITE_DESCRIPTION = 'Welcome to the ALTERNATIVE catalog of mods for Geode, here you can find forbidden or lost mods that are kindly hidden from you. Enjoy the underground~';
