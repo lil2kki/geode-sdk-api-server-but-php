7fdbf3fb4f68ed96d072b1b5e78c31241d29b536
lil2kki <latterrarity70@gmail.com>
2026-07-02 13:23:20 +0400

    Rename UPSTREAM_API constant to UPSTREAM_URL

--- _config.php
+++ _config.php
@@ -4,7 +4,7 @@ const CLIENT_SECRET = 'meows';
 const CALLBACK_URL = 'https://open-geode-index.bccst.ru/callback';
 $ADMIN_USERS = ['lil2kki'];
 
-const UPSTREAM_API = 'https://api.geode-sdk.org';
+const UPSTREAM_URL = 'https://api.geode-sdk.org';
 
 const ICON_URL = 'https://raw.githubusercontent.com/lil2kki/Open-Geode-Index/HEAD/logo.png';
 const SITE_DESCRIPTION = 'Welcome to the ALTERNATIVE catalog of mods for Geode, here you can find forbidden or lost mods that are kindly hidden from you. Enjoy the underground~';
